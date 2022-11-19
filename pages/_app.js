0ce7e677eefa770751c26988b6e45c330376260c
kobayang <naokiod@gmail.com>
2022-11-19 17:36:52 +0900

    Execute lint

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 import "../styles/global.css";
 
-import { StyleSheetManager } from "styled-components";
 import { AuthContextProvider } from "../src/client/foundation/contexts/AuthContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <StyleSheetManager disableCSSOMInjection>
-        <AuthContextProvider>
-          <Component {...pageProps} />
-        </AuthContextProvider>
-      </StyleSheetManager>
+      <AuthContextProvider>
+        <Component {...pageProps} />
+      </AuthContextProvider>
     </>
   );
 }
