4b17ad0539a84651ca7caa79218330bc07af949f
kobayang <naokiod@gmail.com>
2022-11-22 01:03:34 +0900

    Setup cors in api/initialize

--- pages/api/initialize.js
+++ pages/api/initialize.js
@@ -1,6 +1,25 @@
 import axios from "axios";
+import Cors from "cors";
+
+// CORS のミドルウェアを初期化
+const cors = Cors();
+
+// 後続の処理を行う前にミドルウェアの実行を待ち、
+// また、ミドルウェアでエラーが発生したときエラーを投げるためのヘルパーメソッド
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
   await axios.request({
     url: `${process.env.NEXT_PUBLIC_API_HOST}/api/initialize`,
     headers: {
