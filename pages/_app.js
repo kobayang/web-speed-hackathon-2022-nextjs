bbe608f23163f8a4fc007c8b7bd837b5e5917e24
kobayang <naokiod@gmail.com>
2022-11-22 00:34:34 +0900

    Add meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { AuthContextProvider } from "../src/client/foundation/contexts/AuthContext";
 import { GlobalStyle } from "../src/client/foundation/styles/GlobalStyle";
 
@@ -6,6 +7,15 @@ function MyApp({ Component, pageProps }) {
     <>
       <AuthContextProvider>
         <GlobalStyle />
+        <Head>
+          <title>CyberTicket</title>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
         <Component {...pageProps} />
       </AuthContextProvider>
     </>
