9d00563c26c39b11e595247e8989c6ae13703f23
kobayang <naokiod@gmail.com>
2022-11-23 23:51:30 +0900

    Do not use category

--- pages/races/[raceId]/odds.jsx
+++ pages/races/[raceId]/odds.jsx
@@ -3,8 +3,6 @@ import { getRace } from "../../../src/client/apis/getRace";
 import { getRaces } from "../../../src/client/apis/getRaces";
 import { Header } from "../../../src/client/foundation/components/navs/Header/Header";
 import { Odds } from "../../../src/client/foundation/pages/races/Odds/Odds";
-import { RaceCard } from "../../../src/client/foundation/pages/races/RaceCard/RaceCard";
-import { RaceResult } from "../../../src/client/foundation/pages/races/RaceResult/RaceResult";
 
 export default function Component(props) {
   if (props.statusCode) {
@@ -13,9 +11,7 @@ export default function Component(props) {
   return (
     <div>
       <Header />
-      {props.category === "odds" && <Odds {...props} />}
-      {props.category === "race-card" && <RaceCard {...props} />}
-      {props.category === "result" && <RaceResult {...props} />}
+      <Odds {...props} />
     </div>
   );
 }
@@ -32,11 +28,7 @@ export const getStaticPaths = async () => {
   const { races } = await getRaces();
 
   const paths = races.flatMap((race) => {
-    return [
-      { params: { raceId: race.id, category: "odds" } },
-      { params: { raceId: race.id, category: "race-card" } },
-      { params: { raceId: race.id, category: "result" } },
-    ];
+    return [{ params: { raceId: race.id } }];
   });
 
   return {
@@ -45,21 +37,10 @@ export const getStaticPaths = async () => {
   };
 };
 
-// export const getStaticPaths = async () => {
-//   return {
-//     paths: [],
-//     fallback: "blocking",
-//   };
-// };
-
 export async function getStaticProps(context) {
   try {
-    const category = context.params.category;
-    if (!["odds", "race-card", "result"].includes(category)) {
-      throw new Error("Invalid category.");
-    }
     const data = await getRace(context.params.raceId);
-    return { props: { data, category } };
+    return { props: { data } };
   } catch (error) {
     console.log(error);
     return { props: { statusCode: 500 } };
