bebb430589bc5a7b4439ebe8b7b82196e6ff20fe
Naoki Kobayashi <kobayashi@wantedly.com>
2022-11-25 22:16:20 +0900

    Use experimental-edge runtime

--- pages/api/zengin-code.js
+++ pages/api/zengin-code.js
@@ -1,10 +1,19 @@
 import zenginCode from "zengin-code";
 
+export const config = {
+  runtime: "experimental-edge",
+};
+
 const bankList = Object.entries(zenginCode).map(([code, { name }]) => ({
   code,
   name,
 }));
 
-export default async function handler(req, res) {
-  res.status(200).json({ zenginCode, bankList });
+export default async function handler() {
+  return new Response(JSON.stringify({ zenginCode, bankList }), {
+    status: 200,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
 }
