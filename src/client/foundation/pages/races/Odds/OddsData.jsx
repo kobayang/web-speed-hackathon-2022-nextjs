de2bc28ca2cabcb929d105e16931c49067b2f25c
Naoki Kobayashi <kobayashi@wantedly.com>
2022-11-25 21:51:23 +0900

    First render of OddTable by server component

--- src/client/foundation/pages/races/Odds/OddsData.jsx
+++ src/client/foundation/pages/races/Odds/OddsData.jsx
@@ -1,8 +1,11 @@
+import { range, without } from "lodash-es";
 import { Spacer } from "../../../components/layouts/Spacer";
 import { Heading } from "../../../components/typographies/Heading";
 import { Space } from "../../../styles/variables";
 import { OddsRankingList } from "./internal/OddsRankingList";
+import { mapKey } from "./internal/OddsTable/mapkey";
 import { OddsTable } from "./internal/OddsTable/OddsTable";
+import { OddsTableData } from "./internal/OddsTable/OddsTableData";
 
 async function getRaceWithOdds(uuid) {
   const res = await fetch(
@@ -18,22 +21,40 @@ async function getRaceWithOdds(uuid) {
   return res.json();
 }
 
+const firstKey = 1;
+
 export async function OddsData({ raceId, isRaceClosed }) {
   const raceWithOddsData = await getRaceWithOdds(raceId);
+
+  const entries = raceWithOddsData.entries;
+  const odds = raceWithOddsData.trifectaOdds;
+
+  const headNumbers = without(range(1, entries.length + 1), firstKey);
+  const oddsMap = odds
+    .filter((item) => item.key[0] === firstKey)
+    .reduce((acc, cur) => {
+      const [, second, third] = cur.key;
+      acc[mapKey(second, third)] = cur;
+      return acc;
+    }, {});
+
   return (
     <>
       <OddsTable
-        entries={raceWithOddsData.entries}
-        odds={raceWithOddsData.trifectaOdds}
+        entries={entries}
+        odds={odds}
         isRaceClosed={isRaceClosed}
-      />
+      >
+        <OddsTableData
+          isRaceClosed={isRaceClosed}
+          headNumbers={headNumbers}
+          oddsMap={oddsMap}
+        />
+      </OddsTable>
       <Spacer mt={Space * 4} />
       <Heading as="h2">人気順</Heading>
       <Spacer mt={Space * 2} />
-      <OddsRankingList
-        odds={raceWithOddsData.trifectaOdds}
-        isRaceClosed={isRaceClosed}
-      />
+      <OddsRankingList odds={odds} isRaceClosed={isRaceClosed} />
     </>
   );
 }
