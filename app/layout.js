90689adc56d65b217d2201b937e8e521bccd4409
Naoki Kobayashi <kobayashi@wantedly.com>
2022-11-26 17:43:16 +0900

    Fix

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
         <body>
           <div>
             <Header />
-            <Suspense>{children}</Suspense>
+            {children}
           </div>
         </body>
       </html>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import { Suspense } from "react";
 import { Container } from "../src/client/foundation/components/layouts/Container";
 import { Spacer } from "../src/client/foundation/components/layouts/Spacer";
 import { TrimmedImage } from "../src/client/foundation/components/media/TrimmedImage";
 import { Footer } from "../src/client/foundation/components/navs/Footer";
 import { RecentRaceListSection } from "../src/client/foundation/pages/Top/RecentRaceListSection";
 import { Top } from "../src/client/foundation/pages/Top/Top";
+import { Space } from "../src/client/foundation/styles/variables";
 
 export default async function Page() {
   return (
